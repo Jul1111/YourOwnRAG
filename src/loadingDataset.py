faeff05b07880d4a20d88f7209f99f5b459a68f6
Jul1111 <jules.boissel50@gmail.com>
2026-01-13 12:19:49 +0100

    Initialisation du projet et configuration environnement

--- src/loadingDataset.py
+++ src/loadingDataset.py
@@ -3,5 +3,10 @@ dataset = []
 def load_dataset(filename):
     with open(filename,'r') as file:
         dataset = file.readlines()
+
         print(f'Loaded {len(dataset)} entries')
-        return dataset
+
+        #return dataset
+
+load_dataset('tmp/cat-facts.txt')
+
